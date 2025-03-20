733664dc8cf096dc49e0189b0e6f37af8b56a02b
ericc70 <eric.c@codeur.online>
2025-03-20 14:15:18 +0100

    tmpl option xml'

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,8 +3,13 @@
         <?php foreach ($reviews as $review): ?>
             <div class="review">
                 <p><strong><?php echo htmlspecialchars($review['author_name']); ?></strong></p>
+                <?php if ($params->get('show_rating', 1)): ?>
+                    <p>Note: <?php echo $review['rating']; ?>/5</p>
+                <?php endif; ?>
                 <p><?php echo htmlspecialchars($review['text']); ?></p>
-                <p>Note: <?php echo $review['rating']; ?>/5</p>
+                <?php if ($params->get('show_date', 1)): ?>
+                    <p>Date: <?php echo date($params->get('date_format', 'd/m/Y'), strtotime($review['time'])); ?></p>
+                <?php endif; ?>
             </div>
         <?php endforeach; ?>
     </div>
